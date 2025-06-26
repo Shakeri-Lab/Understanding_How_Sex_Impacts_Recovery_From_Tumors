bf59a9c86a98dc6ddc5bab8740772a7192e9edae
Tom Lever <thomas.lever.business@gmail.com>
2025-06-26 12:15:40 -0400

    All tests pass and are efficient.

--- pair_clinical_data_and_stages_of_tumors/test_pipeline_for_pairing_clinical_data_and_stages_of_tumors.py
+++ pair_clinical_data_and_stages_of_tumors/test_pipeline_for_pairing_clinical_data_and_stages_of_tumors.py
@@ -6,51 +6,29 @@ Verify that the output of the pipeline for pairing clinical data and stages of t
 '''
 
 import pandas as pd
+from pandas.testing import assert_frame_equal
 from pathlib import Path
 from pipeline_for_pairing_clinical_data_and_stages_of_tumors import run_pipeline
 import pytest
-import sys
 
 
-DATA_ROOT = Path("/sfs/gpfs/tardis/project/orien/data/aws/24PRJ217UVA_IORIG/Clinical_Data/24PRJ217UVA_NormalizedFiles")
-CSV_CM  = DATA_ROOT / "24PRJ217UVA_20241112_ClinicalMolLinkage_V4.csv"
-CSV_DX  = DATA_ROOT / "24PRJ217UVA_20241112_Diagnosis_V4.csv"
-CSV_MD  = DATA_ROOT / "24PRJ217UVA_20241112_MetastaticDisease_V4.csv"
-KEY_CSV = "ORIEN_Tumor_Staging_Key.csv"
+PATH_TO_CLINICAL_DATA = Path("/sfs/gpfs/tardis/project/orien/data/aws/24PRJ217UVA_IORIG/Clinical_Data/24PRJ217UVA_NormalizedFiles")
+PATH_TO_CLINICAL_MOLECULAR_LINKAGE_DATA = PATH_TO_CLINICAL_DATA / "24PRJ217UVA_20241112_ClinicalMolLinkage_V4.csv"
+PATH_TO_DIAGNOSIS_DATA = PATH_TO_CLINICAL_DATA / "24PRJ217UVA_20241112_Diagnosis_V4.csv"
+PATH_TO_METASTATIC_DISEASE_DATA = PATH_TO_CLINICAL_DATA / "24PRJ217UVA_20241112_MetastaticDisease_V4.csv"
+PATH_TO_KEY = "ORIEN_Tumor_Staging_Key.csv"
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope = "session")
 def output_of_pipeline() -> pd.DataFrame:
     return run_pipeline(
-        path_to_clinical_molecular_linkage_data = CSV_CM,
-        path_to_diagnosis_data = CSV_DX,
-        path_to_metastatic_disease_data = CSV_MD
+        path_to_clinical_molecular_linkage_data = PATH_TO_CLINICAL_MOLECULAR_LINKAGE_DATA,
+        path_to_diagnosis_data = PATH_TO_DIAGNOSIS_DATA,
+        path_to_metastatic_disease_data = PATH_TO_METASTATIC_DISEASE_DATA
     )
 
-def test_that_output_of_pipeline_equals_key(output_of_pipeline: pd.DataFrame) -> None:
-    output_of_pipeline.to_csv("output_of_test_pipeline_for_pairing_clinical_data_and_stages_of_tumors.csv", index = False)
-    orien_tumor_staging_key = pd.read_csv(KEY_CSV).sort_values(by = ["AvatarKey", "ORIENSpecimenID"]).reset_index(drop = True)
-    if output_of_pipeline.shape != orien_tumor_staging_key.shape:
-        print(f"Shapes of output of pipeline {output_of_pipeline.shape} and ORIEN Tumor Staging Key {orien_tumor_staging_key.shape} are mismatched.")
-        sys.exit(1)
-    if list(output_of_pipeline.columns) != list(orien_tumor_staging_key.columns):
-        print(
-            "Output of pipeline and ORIEN Tumor Staging key have different columns.\n" +
-            "Output of pipeline has columns " + ', '.join([name_of_column for name_of_column in output_of_pipeline.columns]) + ".\n" +
-            "ORIEN tumor staging key has columns " + ', '.join([name_of_column for name_of_column in orien_tumor_staging_key.columns]) + "."
-        )
-        sys.exit(1)
+@pytest.fixture(scope = "session")
+def orien_tumor_staging_key() -> pd.DataFrame:
+    return pd.read_csv(PATH_TO_KEY).sort_values(by = ["AvatarKey", "ORIENSpecimenID"]).reset_index(drop = True)
 
-    mask_of_equality = output_of_pipeline.values == orien_tumor_staging_key.values
-    if mask_of_equality.all():
-        print("All corresponding cells in output of pipeline and ORIEN Tumor Staging Key.")
-        return
-
-    tuple_of_arrays_of_row_and_colummn_indices = (~mask_of_equality).nonzero()
-    for index_of_row, index_of_column in zip(*tuple_of_arrays_of_row_and_colummn_indices):
-        name_of_column = orien_tumor_staging_key.columns[index_of_column]
-        value_of_orien_tumor_staging_key = orien_tumor_staging_key.iat[index_of_row, index_of_column]
-        value_of_output_of_pipeline = output_of_pipeline.iat[index_of_row, index_of_column]
-        print(f"Row {index_of_row} | Column '{name_of_column}': {value_of_output_of_pipeline!r}  !=  {value_of_orien_tumor_staging_key!r}. ORIENAvatarKey and DeidSpecimenID are {orien_tumor_staging_key.iat[index_of_row, 0]} and {orien_tumor_staging_key.iat[index_of_row, 1]}.")
-
-    print(f"{len(tuple_of_arrays_of_row_and_colummn_indices[0])} cell(s) in output of pipeline differ from corresponding cells in ORIEN Tumor Staging Key match.")
-    sys.exit(1)
+def test_that_output_of_pipeline_equals_key(output_of_pipeline: pd.DataFrame, orien_tumor_staging_key: pd.DataFrame) -> None:
+    assert_frame_equal(output_of_pipeline, orien_tumor_staging_key)
