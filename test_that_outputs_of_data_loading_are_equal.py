46844adcf891c032443deee4d435970e028017c0
Tom Lever <thomas.lever.business@gmail.com>
2025-07-01 19:23:55 -0400

    Test that outputs of data loading are equal.

--- test_that_outputs_of_data_loading_are_equal.py
+++ test_that_outputs_of_data_loading_are_equal.py
@@ -3,34 +3,16 @@
 Test that ./src/immune_analysis/data_loading.py produces the exact same outputs as sibling reference files.
 '''
 
-from pathlib import Path
+
 import pandas as pd
 from pandas.testing import assert_frame_equal
-import pytest
 
 
-PATH_TO_OUTPUTS_OF_DATA_LOADING = Path("output/data_loading")
+def test_that_outputs_are_equal() -> bool:
+    data_frame_of_melanoma_clinical_data = pd.read_csv("output/data_loading/melanoma_clinical_data.csv")
+    data_frame_of_melanoma_clinical_data_for_comparison = pd.read_csv("output/data_loading/melanoma_clinical_data_for_comparison.csv")
+    assert_frame_equal(data_frame_of_melanoma_clinical_data, data_frame_of_melanoma_clinical_data_for_comparison)
 
-
-@pytest.fixture(scope = "session")
-def pairs() -> list[tuple[str, str]]:
-    pairs = [
-        ("melanoma_clinical_data.csv", "melanoma_clinical_data_for_comparison.csv"),
-        ("melanoma_expression_matrix.csv", "melanoma_expression_matrix_for_comparison.csv")
-    ]
-    return pairs
-
-
-def load(path: Path) -> pd.DataFrame:
-    '''
-    Return a data frame based on a CSV file.
-    '''
-    df = pd.read_csv(path)
-    return df
-
-
-def test_that_outputs_are_equal(pairs) -> bool:
-    for pair in pairs:
-        f1, f2 = (PATH_TO_OUTPUTS_OF_DATA_LOADING / p for p in pair)
-        df1, df2 = map(load, (f1, f2))
-        assert_frame_equal(df1, df2)
+    melanoma_expression_matrix = pd.read_csv("output/data_loading/melanoma_expression_matrix.csv")
+    melanoma_expression_matrix_for_comparison = pd.read_csv("output/data_loading/melanoma_expression_matrix_for_comparison.csv")
+    assert_frame_equal(melanoma_expression_matrix, melanoma_expression_matrix_for_comparison)
