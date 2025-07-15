298582fb794e7448c50419bf2c6a162e8ec3c662
Tom Lever <thomas.lever.business@gmail.com>
2025-07-15 13:38:07 -0400

    Create characterize_immune_cell_infiltrate_in_TME.py.

---
+++ ORIEN_analysis/characterize_immune_cell_infiltrate_in_TME.py
@@ -0,0 +1,15 @@
+'''
+characterize_immune_cell_infiltrate_in_TME.py
+
+TODO: Address Aim 1 in ORIEN Analysis Plan to characterize the immune cell infiltrate in the melanoma Tumor Micro-Environment.
+
+TODO: Address Aim 1.1 to evaluate sex differences in immune cell infiltrate composition.
+'''
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
