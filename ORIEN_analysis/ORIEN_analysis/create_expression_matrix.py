68c3e0bcb5ad72d790814a20fcba1b872ecac32b
Tom Lever <thomas.lever.business@gmail.com>
2025-08-30 21:05:03 -0400

    Ensure 1 to 1 overlap of SLIDs and inclusion of all ORIEN specimen IDs.

--- ORIEN_analysis/ORIEN_analysis/create_expression_matrix.py
+++ ORIEN_analysis/ORIEN_analysis/create_expression_matrix.py
@@ -67,6 +67,13 @@ def create_expression_matrix():
     print(f"Clinical molecular linkage data has shape {clinical_molecular_linkage_data.shape}.")
     print(f"The number of unique specimen IDs is {clinical_molecular_linkage_data['DeidSpecimenID'].nunique()}.")
     print(f"The number of unique sample IDs is {clinical_molecular_linkage_data['RNASeq'].nunique()}.")
+    set_of_SLIDs_in_QC_data = set(QC_data["SLID"])
+    set_of_SLIDs_in_clinical_molecular_linkage_data = set(clinical_molecular_linkage_data["RNASeq"])
+    list_of_SLIDs_in_QC_data_and_not_in_clinical_molecular_linkage_data = sorted(set_of_SLIDs_in_QC_data - set_of_SLIDs_in_clinical_molecular_linkage_data)
+    list_of_SLIDs_in_clinical_molecular_linkage_data_and_not_in_QC_data = sorted(set_of_SLIDs_in_clinical_molecular_linkage_data - set_of_SLIDs_in_QC_data)
+    print(f"List of SLIDS in QC data and not in clinical molecular linkage data is {list_of_SLIDs_in_QC_data_and_not_in_clinical_molecular_linkage_data}.")
+    print(f"List of SLIDS in clinical molecular linkage data and not in QC data is {list_of_SLIDs_in_clinical_molecular_linkage_data_and_not_in_QC_data}.")
+    # There is 1 to 1 overlap between SLIDs in clinical molecular linkage data and QC data before string normalization.
     data_frame_of_filtered_QC_data_and_specimen_IDs = filtered_QC_data.merge(
         clinical_molecular_linkage_data[["RNASeq", "DeidSpecimenID"]],
         how = "left",
@@ -85,6 +92,14 @@ def create_expression_matrix():
     )
     print(f"Output of pipeline for pairing clinical data and stages of tumors has shape {output_of_pipeline_for_pairing_clinical_data_and_stages_of_tumors.shape}.")
     print(f"The number of unique specimen IDs is {output_of_pipeline_for_pairing_clinical_data_and_stages_of_tumors['ORIENSpecimenID'].nunique()}.")
+    set_of_ORIEN_specimen_IDs_in_output_of_pipeline = set(output_of_pipeline_for_pairing_clinical_data_and_stages_of_tumors["ORIENSpecimenID"])
+    set_of_DEID_specimen_IDs_in_clinical_molecular_linkage_data = set(clinical_molecular_linkage_data["DeidSpecimenID"])
+    list_of_ORIEN_specimen_IDs_that_are_not_DEID_specimen_IDs = sorted(set_of_ORIEN_specimen_IDs_in_output_of_pipeline - set_of_DEID_specimen_IDs_in_clinical_molecular_linkage_data)
+    list_of_DEID_specimen_IDs_that_are_not_ORIEN_specimen_IDs = sorted(set_of_DEID_specimen_IDs_in_clinical_molecular_linkage_data - set_of_ORIEN_specimen_IDs_in_output_of_pipeline)
+    print(f"List of ORIEN specimen IDs that are not DEID specimen IDs is {list_of_ORIEN_specimen_IDs_that_are_not_DEID_specimen_IDs}.")
+    #print(f"List of DEID specimen IDs that are not ORIEN specimen IDs is {list_of_DEID_specimen_IDs_that_are_not_ORIEN_specimen_IDs}.")
+    print(f"List of DEID specimen IDs that are not ORIEN specimen IDs is large.")
+    # All ORIEN specimen IDs in output of pipeline are DEID specimen IDs in clinical molecular linkage data. 
     output_of_pipeline_for_cutaneous_specimens = output_of_pipeline_for_pairing_clinical_data_and_stages_of_tumors[
         output_of_pipeline_for_pairing_clinical_data_and_stages_of_tumors["AssignedPrimarySite"] == "cutaneous"
     ]
